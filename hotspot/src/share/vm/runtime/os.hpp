3503b588e28c860c2a2eafb7413949780a7ace1d
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2022-02-16 07:01:37 +0700

    Update os.hpp

--- hotspot/src/share/vm/runtime/os.hpp
+++ hotspot/src/share/vm/runtime/os.hpp
@@ -743,9 +743,6 @@ class os: AllStatic {
   static bool  check_heap(bool force = false);      // verify C heap integrity
   static char* strdup(const char *, MEMFLAGS flags = mtInternal);  // Like strdup
 
-  // mprotect
-  static int mprotect(void *addr, size_t len, int prot);
-
 #ifndef PRODUCT
   static julong num_mallocs;         // # of calls to malloc/realloc
   static julong alloc_bytes;         // # of bytes allocated
@@ -973,11 +970,6 @@ class os: AllStatic {
   // writeable and executable pages. No-op otherwise.
   static inline void current_thread_enable_wx(WXMode mode) {
     current_thread_enable_wx_impl(mode);
-    if (mode == WXExec) {
-      mprotect(GLOBAL_CODE_CACHE_ADDR, GLOBAL_CODE_CACHE_SIZE, PROT_READ | PROT_EXEC);
-    } else {
-      mprotect(GLOBAL_CODE_CACHE_ADDR, GLOBAL_CODE_CACHE_SIZE, PROT_READ | PROT_WRITE);
-    }
   }
 
 #ifndef TARGET_OS_FAMILY_windows
